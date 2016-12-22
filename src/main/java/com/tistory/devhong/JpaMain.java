ff6164a6ff35264b6e1882242c45749c7ab8befb
sungjaeHong <sungjae89@gmail.com>
2016-12-22 16:47:44 +0900

    준영속 상태 예제
    영속성 컨텍스트에 있던 영속상태의 엔티티는, 영속성 컨텍스트가 종료되면
    준영속상태(엔티티에 식별아이디와 값이 있으나 영속성 컨텍스트엔 없는 상태)가 된다.
    
    merge를 사용하면 준영속상태의 엔티티를 영속성컨텍스트가 1차캐시를 확인 후 해당 엔티티가 있으면 반환, 없으면 DB에서 가져와 1차캐시에 저장하고 반환.

--- src/main/java/com/tistory/devhong/JpaMain.java
+++ src/main/java/com/tistory/devhong/JpaMain.java
@@ -12,8 +12,13 @@ import java.util.List;
  * Created by devHong on 2016. 12. 21..
  */
 public class JpaMain {
-    public static void main(String[] args){
-        //엔티티 매니저 팩토리 생성
+
+    static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
+
+    public static void main(String[] args) {
+        // 1차 예제
+
+        /*//엔티티 매니저 팩토리 생성
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook");
         //엔티티 매니저 생성
         EntityManager em = emf.createEntityManager();
@@ -27,10 +32,15 @@ public class JpaMain {
             tx.rollback();
         } finally {
             em.close();
-        }
+        }*/
+        ;
+        Member member = createMember("memberA", "회원");
+        member.setUsername("회원명변경");
+        mergeMember(member);
+
     }
 
-    private static void logic(EntityManager em){
+    private static void logic(EntityManager em) {
         String id = "id1";
         Member member = new Member();
         member.setId(id);
@@ -45,12 +55,64 @@ public class JpaMain {
         member.setUsername("홍성재");
         //한건 조회
         Member findMember = em.find(Member.class, id);
-        System.out.println("findMember=" + findMember.getUsername() + ", age="+ findMember.getAge());
+        System.out.println("findMember=" + findMember.getUsername() + ", age=" + findMember.getAge());
 
         //목록 조회
         List<Member> members = em.createQuery("SELECT m FROM Member m", Member.class).getResultList();
         System.out.println("members.size=" + members.size());
 
-        em.remove(member);
+        //em.remove(member);
     }
+
+    static Member createMember(String id, String username) {
+        //===영속성 컨텍스트1 시작===///
+        System.out.println("id : " + id + ", username : " + username);
+        System.out.println(emf.getProperties());
+        EntityManager em1 = emf.createEntityManager();
+        EntityTransaction tx1 = em1.getTransaction();
+        Member member = new Member();
+
+        tx1.begin();
+        try {
+            member.setId(id);
+            member.setUsername(username);
+            em1.persist(member);
+
+            tx1.commit();
+        } catch (Exception e) {
+            if (tx1.isActive())
+                tx1.rollback();
+        }
+
+        em1.close();    //영속성 컨텍스트 종료
+                        //member 엔티티는 준영속 상태가 됨
+        //===영속성 컨텍트스1 종료===//
+        return member;
+    }
+
+    public static void mergeMember(Member member) {
+        //===영속성 컨텍스트 시작===//
+        EntityManager em2 = emf.createEntityManager();
+        EntityTransaction tx2 = em2.getTransaction();
+
+        tx2.begin();
+        try {
+            Member mergeMember = em2.merge(member);
+            //준영속 상태
+            System.out.println("member = " + member.getUsername());
+
+            //영속 상태
+            System.out.println("mergeMember = " + mergeMember.getUsername());
+
+            System.out.println("em2 contains member = " + em2.contains(member));
+            System.out.println("em2 contains mergeMember = " + em2.contains(mergeMember));  //준영속상태의 객체를 merge했더니 영속상태로 되엇다.
+        }catch(Exception e){
+            if (tx2.isActive())
+                tx2.rollback();
+
+        }
+        em2.close();
+        //===영속성 컨텍스트2 종료===//
+    }
+
 }
